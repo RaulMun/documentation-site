a8bbcb42e16d1b7fa2c1b68f61ba9ff63ab34079
RaulMun <raulmuniz38@gmail.com>
2024-10-26 14:14:10 -0600

    add/routes & basichtml

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,13 @@
-from flask import flask
+from flask import Flask
 
 def create_app():
-    app = flask.Flask(__name__)
+    app = Flask(__name__) 
     app.config['SECRET_KEY'] = 'practicasUTCH'
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
     return app
